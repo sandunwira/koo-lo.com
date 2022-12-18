d7d1010bb7f668ecd299488fedbc9d1a14f52611
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2022-12-18 05:34:22 +0000

    Updated to the latest code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
-  res.status(404)
-  res.sendFile(__dirname + "/404.html")
+	res.status(404)
+	res.sendFile(__dirname + "/404.html")
 });
 
 app.listen(8080);
